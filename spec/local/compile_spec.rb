2587f99aab2c7fce8659e3316ea627ccfd15d78a
Daniel Jones <daniel.jones@engineerbetter.com>
2016-08-10 15:10:00 +0100

    make guarddog buildpack write a file to demonstrate its existence

--- spec/local/compile_spec.rb
+++ spec/local/compile_spec.rb
@@ -1,5 +1,22 @@
+require 'fileutils'
+require 'tmpdir'
+
 describe 'bin/compile' do
-  it 'exits successfully' do
-    expect(system("bin/compile")).to be_truthy
+  let(:tmpdir) { Dir.tmpdir }
+  let(:destination) { File.join(tmpdir, 'compile') }
+  let(:written_file) { File.join(tmpdir, '.guarddog') }
+
+  before(:each) do
+    FileUtils.copy('bin/compile', destination)
+  end
+
+  after(:each) do
+    FileUtils.rm(destination)
+  end
+
+
+  it 'writes a file' do
+    expect(system(destination)).to be_truthy
+    expect(File.exists?(written_file)).to be_truthy
   end
 end
