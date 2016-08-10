143b5f5ce3adfa21470551692a031b357a38adcc
Daniel Jones <daniel.jones@engineerbetter.com>
2016-08-10 16:41:15 +0100

    make bin/compile write to correct location

--- spec/local/compile_spec.rb
+++ spec/local/compile_spec.rb
@@ -3,20 +3,14 @@ require 'tmpdir'
 
 describe 'bin/compile' do
   let(:tmpdir) { Dir.tmpdir }
-  let(:destination) { File.join(tmpdir, 'compile') }
   let(:written_file) { File.join(tmpdir, '.guarddog') }
 
-  before(:each) do
-    FileUtils.copy('bin/compile', destination)
-  end
-
   after(:each) do
-    FileUtils.rm(destination)
+    FileUtils.rm(written_file)
   end
-
 
   it 'writes a file' do
-    expect(system(destination)).to be_truthy
+    expect(system("bin/compile #{tmpdir}")).to be_truthy
     expect(File.exists?(written_file)).to be_truthy
   end
 end
