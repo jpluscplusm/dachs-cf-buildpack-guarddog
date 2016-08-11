9994df7f81d6a0870cc6850a2b16ce47fb25354e
Daniel Jones <daniel.jones@engineerbetter.com>
2016-08-11 17:50:46 +0100

    system test multibuildpack with Ruby

--- spec/system/system_spec.rb
+++ spec/system/system_spec.rb
@@ -10,33 +10,56 @@ describe 'GuardDog with multi-buildpack' do
   let(:org) { ENV.fetch('CF_ORG') }
   let(:space) { ENV.fetch('CF_SPACE') }
   let(:app_domain) { ENV.fetch('APP_DOMAIN') }
-  let(:app_name) { "guarddog-test-app-#{SecureRandom.uuid}" }
+  let(:app_name) { "#{language}-guarddog-test-app-#{SecureRandom.uuid}" }
   let(:multi_buildpack_uri) { ENV.fetch('MULTI_BUILDPACK_URI') }
   let(:guarddog_buildpack_uri) { ENV.fetch('GD_BUILDPACK_URI') }
-  let(:cf_home) { Dir.tmpdir }
-  let(:fixture_dir) { 'spec/system/fixtures/hello-python-web' }
-  let(:multi_buildpack_conf_path) { File.join(fixture_dir, '.buildpacks') }
+  let(:multi_buildpack_conf_path) { File.join(app_path, '.buildpacks') }
+
 
   before(:each) do
-    ENV['CF_HOME'] = cf_home
-    File.open(multi_buildpack_conf_path, 'w') { |file|
-      file.puts guarddog_buildpack_uri
-      file.puts 'https://github.com/cloudfoundry/python-buildpack.git'
-    }
+    @cf_home = Dir.mktmpdir
+    ENV['CF_HOME'] = @cf_home
+
+    `cf login -a #{cf_api} -u #{cf_username} -p #{cf_password} -o #{org} -s #{space} --skip-ssl-validation`
+    expect($?.success?).to be_truthy, 'Login should have worked'
   end
 
   after(:each) do
     `cf delete -f #{app_name}`
-    FileUtils.rm_rf cf_home
+    FileUtils.rm_rf @cf_home
     FileUtils.rm_rf multi_buildpack_conf_path
   end
 
-  it 'allows regular apps to run whilst still providing .guarddog file' do
-    `cf login -a #{cf_api} -u #{cf_username} -p #{cf_password} -o #{org} -s #{space} --skip-ssl-validation`
-    expect($?.success?).to be_truthy, 'Login should have worked'
+  context 'when pushing a Python app' do
+    let(:language) { 'python' }
+    let(:app_path) { 'spec/system/fixtures/hello-python-web' }
 
-    `cf push #{app_name} -p spec/system/fixtures/hello-python-web -b #{multi_buildpack_uri} --no-start`
-    `cf set-health-check #{app_name} none`
+    it 'allows regular apps to run whilst still providing .guarddog file' do
+      write_buildpacks_file(app_path, 'https://github.com/cloudfoundry/python-buildpack.git')
+      expect_app_returns_hello_world(app_path)
+    end
+  end
+
+  context 'when pushing a Ruby app' do
+    let(:language) { 'ruby' }
+    let(:app_path) { 'spec/system/fixtures/ruby-hello-world' }
+
+    it 'allows a ruby app to run whilst providing a .guarddog file' do
+      write_buildpacks_file(app_path, 'https://github.com/cloudfoundry/ruby-buildpack.git')
+      expect_app_returns_hello_world(app_path)
+    end
+  end
+
+  def write_buildpacks_file(fixture_dir, buildpack_url)
+    File.open(multi_buildpack_conf_path, 'w') { |file|
+      file.puts guarddog_buildpack_uri
+      file.puts buildpack_url
+    }
+  end
+
+  def expect_app_returns_hello_world(app_path)
+    system("cf push #{app_name} -p #{app_path} -b #{multi_buildpack_uri} --no-start")
+    system("cf set-health-check #{app_name} none")
     push_success = system("cf start #{app_name}")
     expect(push_success).to be_truthy
 
@@ -44,7 +67,7 @@ describe 'GuardDog with multi-buildpack' do
 
     response = RestClient::Request.execute(method: :get, url: "https://#{app_name}.#{app_domain}", verify_ssl: OpenSSL::SSL::VERIFY_NONE)
     expect(response.code).to be(200)
-    expect(response.body).to eq('Hello, World!')
+    expect(response.body).to include('Hello, World!')
 
     output = `cf ssh #{app_name} --command "ls -la app/"`
     expect(output).to include('.guarddog')
