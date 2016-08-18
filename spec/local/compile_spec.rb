cfa7aa534b74405450f844ea388ec04b2e9cbead
Dan Young <dan.young@engineerbetter.com>
2016-08-18 16:24:49 +0100

    local test for haproxy download success

--- spec/local/compile_spec.rb
+++ spec/local/compile_spec.rb
@@ -2,15 +2,24 @@ require 'fileutils'
 require 'tmpdir'
 
 describe 'bin/compile' do
-  let(:tmpdir) { Dir.tmpdir }
-  let(:written_file) { File.join(tmpdir, '.guarddog') }
 
-  after(:each) do
-    FileUtils.rm(written_file)
+  before(:all) do
+    @tmpdir = Dir.mktmpdir
+    @written_file = File.join(@tmpdir, '.guarddog')
+    @haproxy = File.join(@tmpdir, 'haproxy')
+    system("ruby bin/compile.rb #{@tmpdir}")
+  end
+
+  after(:all) do
+    FileUtils.rm_rf(@tmpdir)
   end
 
   it 'writes a file' do
-    expect(system("bin/compile #{tmpdir}")).to be_truthy
-    expect(File.exists?(written_file)).to be_truthy
+    expect(File.exists?(@written_file)).to be_truthy
   end
+
+  it 'downloads haproxy' do
+    expect(File.exists?(@haproxy)).to be_truthy
+  end
+
 end
