f6680df48757a541a72aa4281be21649d1876344
Colin Simmons <colin.r.simmons@gmail.com>
2016-08-18 14:47:26 +0100

    extract command helper methods to spec_helper

--- spec/integration/packaged_buildpack_spec.rb
+++ spec/integration/packaged_buildpack_spec.rb
@@ -77,15 +77,4 @@ describe 'GuardDog buildpack alone' do
       end
     end
   end
-
-  def expect_command_to_succeed(command)
-    system(command)
-    expect($?.success?).to be_truthy
-  end
-
-  def expect_command_to_succeed_and_output(command, expected)
-    output = `#{command}`
-    expect($?.success?).to be_truthy
-    expect(output).to include(expected)
-  end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+def expect_command_to_succeed(command)
+  system(command)
+  expect($?.success?).to be_truthy
+end
+
+def expect_command_to_succeed_and_output(command, expected)
+  output = `#{command}`
+  expect($?.success?).to be_truthy
+  expect(output).to include(expected)
+end
