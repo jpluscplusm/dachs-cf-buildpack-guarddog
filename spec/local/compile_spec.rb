f281160e49e06ed221adbf32ef77b52db00707d1
Colin Simmons <colin.r.simmons@gmail.com>
2016-08-18 17:11:56 +0100

    clarify nginx download spec

--- spec/local/compile_spec.rb
+++ spec/local/compile_spec.rb
@@ -4,15 +4,15 @@ require 'tmpdir'
 describe 'bin/compile' do
 
   before(:all) do
-    @tmpdir = Dir.mktmpdir('app')
+    @app_dir = Dir.mktmpdir('app')
     @cache_dir = Dir.mktmpdir('cache')
-    @guarddog_file = File.join(@tmpdir, '.guarddog')
-    @haproxy = File.join(@tmpdir, 'haproxy')
-    system("ruby bin/compile #{@tmpdir} #{@cache_dir}")
+    @guarddog_file = File.join(@app_dir, '.guarddog')
+    @haproxy = File.join(@app_dir, 'haproxy')
+    system("ruby bin/compile #{@app_dir} #{@cache_dir}")
   end
 
   after(:all) do
-    FileUtils.rm_rf(@tmpdir)
+    FileUtils.rm_rf(@app_dir)
   end
 
   it 'writes the .guarddog file' do
@@ -23,12 +23,12 @@ describe 'bin/compile' do
     expect(File.exists?(@haproxy)).to be_truthy
   end
 
-  it 'downloads nginx' do
+  it 'downloads nginx tarball' do
     expect(File.exists?(File.join(@cache_dir, 'nginx.tgz'))).to be_truthy
   end
 
-  it 'untars nginx' do
-    expect(Dir.exists?(File.join(@tmpdir, 'nginx'))).to be_truthy
-    expect(File.exists?(File.join(@tmpdir, 'nginx', 'sbin', 'nginx'))).to be_truthy
+  it 'untars nginx executable' do
+    expect(Dir.exists?(File.join(@app_dir, 'nginx'))).to be_truthy
+    expect(File.exists?(File.join(@app_dir, 'nginx', 'sbin', 'nginx'))).to be_truthy
   end
 end
