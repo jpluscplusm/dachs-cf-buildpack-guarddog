b9f70e7fd2d264ac90f32401f73bbe9b5e72205a
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-08-26 11:38:58 +0100

    remove whitespace

--- spec/unit/downloader_spec.rb
+++ spec/unit/downloader_spec.rb
@@ -6,11 +6,11 @@ describe Downloader do
     it 'downloads haproxy' do
       stub = stub_request(:get, 's3-eu-west-1.amazonaws.com/dachs-haproxy-build/haproxy').
         to_return(body: 'file-contents')
-      
+
       file = double('file')
       expect(Downloader).to receive(:open).with('destination/haproxy', 'wb').and_yield(file)
       expect(file).to receive(:write).with('file-contents')
-      
+
       Downloader.download('s3-eu-west-1.amazonaws.com', '/dachs-haproxy-build/haproxy', 'destination/haproxy')
       expect(stub).to have_been_requested
     end
